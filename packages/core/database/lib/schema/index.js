98e3ffba5da9ae76126bf8dba3f0d698c26633d2
Deer403 <919187569@qq.com>
2023-06-21 09:09:31 +0800

    add schema compare with env is dev

--- packages/core/database/lib/schema/index.js
+++ packages/core/database/lib/schema/index.js
@@ -79,9 +79,13 @@ const createSchemaProvider = (db) => {
 
       const { hash: oldHash } = oldSchema;
       const hash = await this.schemaStorage.hashSchema(schema);
-
+      const env = process.env.NODE_ENV;
       if (oldHash !== hash) {
-        throw new Error("如果不是预期，请检查数据库是否需要更新，如果是预期，请注释这行代码，执行完后再开放注释，这是风险操作");
+        if (env !== 'production') {
+          throw new Error(
+            '如果不是预期，请检查数据库是否需要更新，如果是预期，请注释这行代码，执行完后再开放注释，这是风险操作'
+          );
+        }
         debug('Schema changed');
 
         return this.syncSchema();
